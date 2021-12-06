f2b5bf4dca2ce568dff0648a1bf1e919aebb1d53
Irene Ntungwe <62404687+ntungwe-90@users.noreply.github.com>
2021-12-06 02:41:05 -0800

    Added JS events

--- main.js
+++ main.js
@@ -1,12 +1,51 @@
-const header = document.querySelector('header');
-window.addEventListener('scroll', () => {
-    if (window.scrollY >header.offsetHeight +60){
-        header.classList.add('sticky')
+// const header = document.querySelector('header');
 
-    }else{
-        header.classList.remove('sticky')
-    }
-})
+// created to input the menu directly from the page
+// const input =document.querySelector('input');
+// const btn = document.querySelector('.btn');
+// const lists = document.querySelector('.lists')
+
+
+
+// btn.addEventListener('click', () =>{
+// const inputSearch = input.value;
+
+// if (inputSearch.length < 5) {
+//   alert('too short length try encore')
+//   input.value = "";
+// }else {
+//   const el = document.createElement('li')
+//   el.innerText =inputSearch;
+//   lists.appendChild(el);
+//   input.value =''
+// }
+// })
+
+// assignment
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+const btn = document.getElementById('btn');
+const modal = document.getElementById('pop-up');
+
+btn.addEventListener('click', () =>{
+  modal.style.display = "block";
+
+ 
+  
+});
+
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY > header.offsetHeight +60){
+//         header.classList.add('sticky')
+
+//     }else{
+//         header.classList.remove('sticky')
+//     }
+// })
 
 
 
@@ -22,12 +61,12 @@ window.addEventListener('scroll', () => {
 // })
 
 
-function myburger() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "header") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "header";
-  }
-}
+// function myburger() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "header") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "header";
+//   }
+// }
 
